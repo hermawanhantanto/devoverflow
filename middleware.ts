3fa983e7557bc5989b79b0a298935ef68f52371b
Hermawan Hantanto <hermawan.hant@gmail.com>
2024-05-22 14:40:36 +0700

    Fixing Client Error

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,19 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
   publicRoutes: [
-    '/',
-    '/api/webhook',
-    'question/:id',
-    '/tags',
-    '/tags/:id',
-    '/profile/:id',
-    '/community',
-    '/jobs'
+    "/",
+    "/api/webhook",
+    "question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
   ],
-  ignoredRoutes: [
-    '/api/webhook', '/api/chatgpt'
-  ]
+  ignoredRoutes: ["/api/chatgpt"],
 });
- 
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
